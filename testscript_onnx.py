a8d11d36ff1f8f2c34e72658baadf184ed87120a
Marten Kreis <martenkre@gmail.com>
2024-11-07 14:16:08 +0100

    Removed list argument from parser - onnx testscript

--- testscript_onnx.py
+++ testscript_onnx.py
@@ -411,7 +411,7 @@ def test(data,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='test.py')
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', type=str, default='yolov7.pt', help='model.pt path(s)')
     parser.add_argument('--data', type=str, default='data/coco.yaml', help='*.data path')
     parser.add_argument('--batch-size', type=int, default=1, help='size of each image batch')
     parser.add_argument('--img-size', type=int, default=1024, help='inference size (pixels)')
