9c7f62b2689dd9fee384096a123009ab9fc2999b
PiaMoe <0302pi@gmail.com>
2025-05-27 09:25:01 +0200

    log predictions

--- models/yolo.py
+++ models/yolo.py
@@ -171,7 +171,7 @@ class IDetect(nn.Module):
 
             # log model outputs
             raw = x[i].detach().cpu()  # shape: (bs, na, ny, nx, no)
-            log_predictions(raw, self.epoch, self.batch_i, output_dir="../preds/", sample_prob=0.01, col_names=["x", "y", "w", "h", "obj", "class_0", "distance", "heading"])
+            log_predictions(raw, self.epoch, self.batch_i, output_dir="preds/", sample_prob=0.01, col_names=["x", "y", "w", "h", "obj", "class_0", "distance", "heading"])
             if not self.training:  # inference
                 if self.grid[i].shape[2:4] != x[i].shape[2:4]:
                     self.grid[i] = self._make_grid(nx, ny).to(x[i].device)
