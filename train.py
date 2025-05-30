88fba3ad251de7b6838db378b404203384188e61
PiaMoe <0302pi@gmail.com>
2025-05-30 11:38:16 +0200

    fix head loss bug

--- train.py
+++ train.py
@@ -301,7 +301,6 @@ def train(hyp, opt, device, tb_writer=None):
     maps = np.zeros(nc)  # mAP per class
     results = (0, 0, 0, 0, 0, 0, 0)  # P, R, mAP@.5, mAP@.5-.95, val_loss(box, obj, cls)
     scheduler.last_epoch = start_epoch - 1  # do not move
-    #scaler = amp.GradScaler(enabled=cuda)
     scaler = amp.GradScaler('cuda', enabled=cuda)
     compute_loss_ota = ComputeLossOTA(model)  # init loss class
     compute_loss = ComputeLoss(model)  # init loss class
@@ -368,7 +367,7 @@ def train(hyp, opt, device, tb_writer=None):
                     imgs = F.interpolate(imgs, size=ns, mode='bilinear', align_corners=False)
 
             # Forward
-            with amp.autocast(enabled=cuda):
+            with amp.autocast('cuda', enabled=cuda):
                 pred = model(imgs, epoch=epoch, batch_i=i)  # forward
                 if 'loss_ota' not in hyp or hyp['loss_ota'] == 1:
                     loss, loss_items = compute_loss_ota(pred, targets.to(device), imgs)  # loss scaled by batch_size
