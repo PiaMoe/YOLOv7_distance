3553eff8316b3fdbe822e54cfa67d605edce10b4
PiaMoe <0302pi@gmail.com>
2025-06-30 11:35:18 +0200

    detect and tracking

--- tracking.py
+++ tracking.py
@@ -165,9 +165,11 @@ def detect(save_img=False):
         # Inference
         t1 = time_synchronized()
         with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
-            pred = model(img, augment=opt.augment)[0]
+            detect_pred, distance_pred, heading_pred = model(img, augment=opt.augment)[0]
         t2 = time_synchronized()
 
+        # combine outputs
+        pred = torch.cat([detect_pred, distance_pred, heading_pred], dim=-1)
         # Apply NMS
         pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
         t3 = time_synchronized()
