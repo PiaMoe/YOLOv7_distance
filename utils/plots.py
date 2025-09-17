2dc53755210245f3061b611f83e747d361940b4f
PiaMoe <0302pi@gmail.com>
2025-09-17 16:40:05 +0200

    Revert back to 953ec015

--- utils/plots.py
+++ utils/plots.py
@@ -63,13 +63,13 @@ def plot_one_box(x, img, color=None, label=None, heading=None, line_thickness=3,
     tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
     color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-    cv2.rectangle(img, c1, c2, color, thickness=2, lineType=cv2.LINE_AA)
+    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
     if label:
         tf = max(tl - 1, 1)  # font thickness
-        t_size = cv2.getTextSize(label, 0, fontScale=tl / 2, thickness=tf)[0]
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
         c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
         cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
-        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 2, textcolor, thickness=tf, lineType=cv2.LINE_AA)
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, textcolor, thickness=tf, lineType=cv2.LINE_AA)
     if heading:
         # Plots arrow pointing in heading direction
         center_x = int((x[0] + x[2]) / 2)
@@ -84,7 +84,7 @@ def plot_one_box(x, img, color=None, label=None, heading=None, line_thickness=3,
         # calculate direction
         end_x = int(center_x + arrow_length * math.sin(rad))
         end_y = int(center_y + arrow_length * math.cos(rad))
-        cv2.arrowedLine(img, (center_x, center_y), (end_x, end_y), color, thickness=2, line_type=cv2.LINE_AA,
+        cv2.arrowedLine(img, (center_x, center_y), (end_x, end_y), color, thickness=tl, line_type=cv2.LINE_AA,
                         tipLength=0.2)
 
 
